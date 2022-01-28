bac1c574fe4afa8d85bff0f6885b3562fdb08ea2
rolledegg <wkadlf2310@gmail.com>
2022-01-28 20:09:13 +0900

    2022/01/28

---
+++ codingTestStudy/2022_01_27_sorting.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    arr=[3,5,1,8,9,0,2,4]
+    min = arr[0]
+    for i in range(len(arr)):
+        minOindex = i
+        for j in range(i,len(arr)):
+            arr[
+
+    print(arr)
